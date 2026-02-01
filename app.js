98168460523fe86b6d583b94a1791b35e111f8bf
Radhika <radhikajha486@gmail.com>
2026-02-01 22:35:59 +0530

    DriveClone

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const userRouter = require("./routes/user.routes");
+const dotenv = require("dotenv");
+const connectToDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+
+
+dotenv.config();
+connectToDB();
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/user", userRouter);
+
+app.listen(8080, (req, res) => {
+  console.log("app is listening on port 8080");
+});
